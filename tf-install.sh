dbff51a7ec22fa26eed73f654396b73e284b9fd2
hoalongnatsu <hoalongnatsu@gmail.com>
2022-06-30 13:04:23 +0700

    init

--- tf-install.sh
+++ tf-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+apt-get update && apt-get install -y gnupg software-properties-common curl
 
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
-sudo apt-get update && sudo apt-get install terraform
+apt-get update && apt-get install terraform
