55d8d473f11dfd7a65257026967929a3f81f7d63
huannv93 <64687828+huannv93@users.noreply.github.com>
2022-07-28 15:44:14 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,8 @@ terraform {
     key            = "terraform-jenkins"
     region         = "us-west-2"
     encrypt        = true
-    role_arn       = "arn:aws:iam::<ACCOUNT_ID>:role/Terraform-SeriesS3BackendRole"
+#     role_arn       = "arn:aws:iam::<ACCOUNT_ID>:role/Terraform-SeriesS3BackendRole"
+    role_arn       = "arn:aws:iam::830525526169:role/Terraform-SeriesS3BackendRole"
     dynamodb_table = "terraform-series-s3-backend"
   }
 }
